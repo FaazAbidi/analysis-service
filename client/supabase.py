95b47b23cb0c1f1c4272380f9ae83f652ea905b6
usman <usman.ghani4501@gmail.com>
2025-05-10 16:39:04 +0200

    Implemented write file on supabase

--- client/supabase.py
+++ client/supabase.py
@@ -5,3 +5,6 @@ url: str = os.environ.get('SUPABASE_URL', '<url>')
 key: str = os.environ.get('SUPBASE_KEY', '<key>')
 
 supabase: Client = create_client(supabase_url=url, supabase_key=key)
+
+def get_supabase_client():
+    return supabase
