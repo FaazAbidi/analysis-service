b912009b61ef3b1dd61c0ac3863379ea93fe443e
Ahasn Habib <ash.habib61@gmail.com>
2025-06-29 17:39:57 +0200

    Fixed issues in pre analysis

--- analysis/pre_analysis/pa_data_transformation.R
+++ analysis/pre_analysis/pa_data_transformation.R
@@ -57,6 +57,7 @@ check_skewness <- function(
     if (length(numeric_column) == 0) next
     
     skew_value <- skewness(numeric_column, na.rm = TRUE)
+    if (is.na(skew_value) || is.nan(skew_value)) next
     
     if (abs(skew_value) > threshold_check_skewness) {
       skewed_columns <- c(skewed_columns, col_name)
